b2345fd99cbff9531e7ce21f9fcf58bf577ffec1
Mijeong Ban <mban1@stevens.edu>
2018-01-01 11:00:00 +0900

    post list using ArrayList

--- WebContent/forum-main.jsp
+++ WebContent/forum-main.jsp
@@ -1,6 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ page import="java.io.PrintWriter" %>
+<%@ page import="post.postDAO" %>
+<%@ page import="post.post" %>
+<%@ page import="java.util.ArrayList" %>
 <!DOCTYPE html>
 <html>
 <head>
--- src/post/postDAO.java
+++ src/post/postDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class postDAO {
 	
@@ -68,6 +69,44 @@ public class postDAO {
 			e.printStackTrace();
 		}
 		return -1; // database error		
+	}
+	
+	public ArrayList<post> getList(int pageNumber) {
+		String SQL = "SELECT * FROM posts WHERE postID < ? AND postAvailable = 1 ORDER BY postID DESC LIMIT 10";
+		ArrayList<post> list = new ArrayList<post>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				post post = new post();
+				post.setPostID(rs.getInt(1));
+				post.setPostTitle(rs.getString(2));
+				post.setUserID(rs.getString(3));
+				post.setPostDate(rs.getString(4));
+				post.setPostContent(rs.getString(5));
+				post.setPostAvailable(rs.getInt(6));
+				list.add(post);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public boolean nextPage(int pageNumber) {
+		String SQL = "SELECT * FROM posts WHERE postID < ? AND postAvailable = 1";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;		
 	}
 
 }
